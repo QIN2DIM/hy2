b708ebbc6cb1b4691a801bb1b98650aa6c146361
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-10-17 17:51:40 +0800

    update(nekolink-to-clash): dataclass style

--- nekolink_to_clash.py
+++ nekolink_to_clash.py
@@ -5,8 +5,9 @@
 # Description:
 import base64
 import json
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List, Dict, Any
+from typing import List
 from urllib.parse import urlparse
 
 import yaml
@@ -14,61 +15,75 @@ import yaml
 # 从 NekoRay(v3.23) 批量导出 hysteria2 节点的 NekoLink 分享链接
 # nekoray://custom#eyJf ....
 neko_links_put = """
-
 """
 
 
-def parse_neko_links(neko_links: str):
-    neko_links = [
-        urlparse(i).fragment for i in neko_links.split("\n") if i.startswith("nekoray://")
-    ]
-    proxies = []
-    for link in neko_links:
-        metadata = json.loads(base64.b64decode(link.strip()).decode("utf8"))
+@dataclass
+class ProxyNode:
+    name: str = field(default=str)
+    type: str = "hysteria2"
+    server: str = field(default=str)
+    port: int = field(default=int)
+    password: str = field(default=str)
+    sni: str = field(default=str)
+    skip_cert_verify: bool = field(default=bool)
+
+    @classmethod
+    def from_neko(cls, link: str):
+        code_part = urlparse(link.strip()).fragment
+        metadata = json.loads(base64.b64decode(code_part).decode("utf8"))
         cs = json.loads(metadata["cs"])
         server, port = cs["server"].split(":")
-        proxy_node = {
-            "name": cs["tls"]["sni"],
-            "type": "hysteria2",
-            "server": server,
-            "port": int(port),
-            "password": cs["auth"],
-            "sni": cs["tls"]["sni"],
-            "skip-cert-verify": cs["tls"]["insecure"],
-        }
-        proxies.append(proxy_node)
+        return cls(
+            name=cs["tls"]["sni"],
+            server=server,
+            port=int(port),
+            password=cs["auth"],
+            sni=cs["tls"]["sni"],
+            skip_cert_verify=cs["tls"]["insecure"],
+        )
 
-    return proxies
-
-
-def flat_proxy_group(proxies: List[Dict[str, Any]]) -> List[dict]:
-    group = {"name": "PROXY", "type": "select", "proxies": []}
-    for proxy_node in proxies:
-        group["proxies"].append(proxy_node["name"])
-
-    return [group]
+    def to_dict(self):
+        c = self.__dict__.copy()
+        del c["skip_cert_verify"]
+        c["skip-cert-verify"] = self.skip_cert_verify
+        return c
 
 
-def to_yaml(proxies, groups):
-    template_path = Path("templates/clash_config.yaml")
-    config = yaml.safe_load(template_path.read_text(encoding="utf8"))
-    config.update({"proxies": proxies, "proxy-groups": groups})
+@dataclass
+class ProxyGroup:
+    name: str = "PROXY"
+    type: str = "select"
+    proxies: List[str] = field(default_factory=list)
 
-    output_path = "clash_verge_config.yaml"
-    with open(output_path, "w", encoding="utf8") as file:
-        yaml.safe_dump(config, file, sort_keys=False)
+    @classmethod
+    def from_proxies(cls, proxies: List[ProxyNode], name: str = "PROXY", type_: str = "select"):
+        return cls(name=name, type=type_, proxies=[proxy_node.name for proxy_node in proxies])
 
-    return output_path
+
+def parse_neko_links(neko_links: str) -> List[ProxyNode]:
+    neko_links = [i for i in neko_links.split("\n") if i.startswith("nekoray://")]
+    return [ProxyNode.from_neko(link) for link in neko_links]
 
 
 def run():
+    template_path = Path("templates/clash_config.yaml")
+    output_path = Path("clash_verge_config.yaml")
+
     if not neko_links_put.strip():
         return
 
     proxies = parse_neko_links(neko_links_put)
-    groups = flat_proxy_group(proxies)
-    output_path = to_yaml(proxies, groups)
+    group = ProxyGroup.from_proxies(proxies)
 
+    proxies = [p.to_dict() for p in proxies]
+    groups = [group.__dict__]
+
+    config = yaml.safe_load(template_path.read_text(encoding="utf8"))
+    config.update({"proxies": proxies, "proxy-groups": groups})
+    output_path.write_text(yaml.safe_dump(config, sort_keys=False), encoding="utf8")
+
+    print(config)
     print(f">> output --> {output_path}")
 
 
